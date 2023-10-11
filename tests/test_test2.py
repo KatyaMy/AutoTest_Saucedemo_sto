263764839fffa48d322d639acb0f0b5066142fdd
KatyaMy <20FamGam20@gmail.com>
2023-10-10 21:38:48 -0400

    First commit

--- tests/test_test2.py
+++ tests/test_test2.py
@@ -1,18 +1,9 @@
-from selenium import webdriver
+
 from selenium.webdriver.common.by import By
 import time
 
 
-def test_remove_from_cart(driver, open_page, login_by_standard_user):
-    # username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
-    # username_field.send_keys("standard_user")
-    #
-    # password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
-    # password_field.send_keys("secret_sauce")
-    #
-    # login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
-    # login_button.click()
-
+def test_remove_from_cart(driver, login_user):
     # adding item to the cart
     add_to_cart_button = driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]')
     add_to_cart_button.click()
@@ -35,4 +26,4 @@ def test_remove_from_cart(driver, open_page, login_by_standard_user):
     assert cart_badge.is_displayed() is False
     # assert item.is_not_displayed()
 
-    driver.quit()
+
