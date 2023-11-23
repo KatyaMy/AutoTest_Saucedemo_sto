c3cb8edf679d4173f69e62fa70d0fceee208ce2b
KatyaMy <20FamGam20@gmail.com>
2023-11-22 23:39:15 -0500

    one more time

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,3 +1,6 @@
+import pytest
+from selenium.webdriver.common.by import By
+
 print('hello world')
 print('Hi')
 
@@ -11,3 +14,9 @@ print('Hi')
 #         results.append(f"Товар '{product_name}' не имеет названия или цены")
 #     for result in results:
 #         print(result)
+
+@pytest.mark.skip("this test is copy need for check actions")
+def test_check_actrions(driver, open_page, correct_user):
+    driver.find_element(By.CSS_SELECTOR, '.shopping_cart_link').click()
+    driver.find_element(By.XPATH, '//*[@id = "checkout"]').click()
+    assert driver.current_url == 'https://www.saucedemo.com/checkout-step-one.html', "Page Not found"
