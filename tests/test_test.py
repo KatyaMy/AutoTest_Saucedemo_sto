e88278a9653d1ba17dc3ee9043ae46a785b7d711
KatyaMy <20FamGam20@gmail.com>
2023-11-24 10:19:25 -0500

    actions_01

--- tests/test_test.py
+++ tests/test_test.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 print('hello world')
 print('Hi')
 
+
 # results = []
 # for product in product_elements:
 #     product_name = product.find_element(By.CLASS_NAME, "product-name").text
@@ -14,6 +15,13 @@ print('Hi')
 #         results.append(f"Товар '{product_name}' не имеет названия или цены")
 #     for result in results:
 #         print(result)
+
+@pytest.mark.skip("this test is copy need for check actions")
+def test_check_actrions(driver, open_page, correct_user):
+    driver.find_element(By.CSS_SELECTOR, '.shopping_cart_link').click()
+    driver.find_element(By.XPATH, '//*[@id = "checkout"]').click()
+    assert driver.current_url == 'https://www.saucedemo.com/checkout-step-one.html', "Page Not found"
+
 
 @pytest.mark.skip("this test is copy need for check actions")
 def test_check_actrions(driver, open_page, correct_user):
