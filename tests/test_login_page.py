a53dc3e4e6f2cb3e45e7347aa84e343a136b79f2
KatyaMy <20FamGam20@gmail.com>
2023-11-24 22:19:32 -0500

    add new function

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -29,3 +29,10 @@ def test_check_actrions(driver, open_page, correct_user):
     driver.find_element(By.CSS_SELECTOR, '.shopping_cart_link').click()
     driver.find_element(By.XPATH, '//*[@id = "checkout"]').click()
     assert driver.current_url == 'https://www.saucedemo.com/checkout-step-one.html', "Page Not found"
+
+
+@pytest.mark.skip("this test is copy need for check actions")
+def test_check_actrions(driver, open_page, correct_user):
+    driver.find_element(By.CSS_SELECTOR, '.shopping_cart_link').click()
+    driver.find_element(By.XPATH, '//*[@id = "checkout"]').click()
+    assert driver.current_url == 'https://www.saucedemo.com/checkout-step-one.html', "Page Not found"
